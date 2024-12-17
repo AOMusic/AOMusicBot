40ef21d17cc9894d9f1ba00452b6dba484c000be
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-17 17:18:46 +0530

    Update misc.py

--- AnonXMusic/misc.py
+++ AnonXMusic/misc.py
@@ -16,9 +16,7 @@ HAPP = None
 _boot_ = time.time()
 
 def is_heroku():
-    """
-    Check if the app is running on Heroku.
-    
+    """ Check if the app is running on Heroku.
     Returns:
         bool: True if the app is running on Heroku, False otherwise.
     """
@@ -42,17 +40,13 @@ XCB = [
 ]
 
 def dbb():
-    """
-    Initialize the local database.
-    """
+    """ Initialize the local database. """
     global db
     db = {}
     LOGGER(__name__).info(f"Local Database Initialized.")
 
 async def sudo():
-    """
-    Load sudoers from the database.
-    """
+    """ Load sudoers from the database. """
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
     sudoersdb = mongodb.sudoers
@@ -60,20 +54,18 @@ async def sudo():
     sudoers = [] if not sudoers else sudoers["sudoers"]
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"},
-            {"$set": {"sudoers": sudoers}},
-            upsert=True,
-        )
+    await sudoersdb.update_one(
+        {"sudo": "sudo"},
+        {"$set": {"sudoers": sudoers}},
+        upsert=True,
+    )
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
     LOGGER(__name__).info(f"Sudoers Loaded.")
 
 def heroku():
-    """
-    Configure the Heroku app.
-    """
+    """ Configure the Heroku app. """
     global HAPP
     if is_heroku:
         if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
@@ -84,4 +76,4 @@ def heroku():
             except BaseException:
                 LOGGER(__name__).warning(
                     f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
-)
+    )
