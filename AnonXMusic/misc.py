ebeba6b79bd4242c2bf6a2b997f83d51222b71a2
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-17 18:55:03 +0530

    Update misc.py

--- AnonXMusic/misc.py
+++ AnonXMusic/misc.py
@@ -1,15 +1,47 @@
-import os
-import asyncio
-from .logging import LOGGER
+import socket
+import time
+
+import heroku3
 from pyrogram import filters
-from . import config  # config ko import kiya hai
+
+import config
+from AnonXMusic.core.mongo import mongodb
+
+from .logging import LOGGER
+
+SUDOERS = filters.user()
+
+HAPP = None
+_boot_ = time.time()
+
+
 def is_heroku():
-    return os.environ.get("DYNO", None) is not None
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "master",
+]
+
+
 def dbb():
     global db
     db = {}
     LOGGER(__name__).info(f"Local Database Initialized.")
-SUDOERS = filters.user()  
+
+
 async def sudo():
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
@@ -18,12 +50,17 @@ async def sudo():
     sudoers = [] if not sudoers else sudoers["sudoers"]
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
-    await sudoersdb.update_one(
-        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
-    )
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
+    LOGGER(__name__).info(f"Sudoers Loaded.")
+
+
 def heroku():
     global HAPP
     if is_heroku:
@@ -31,12 +68,8 @@ def heroku():
             try:
                 Heroku = heroku3.from_key(config.HEROKU_API_KEY)
                 HAPP = Heroku.app(config.HEROKU_APP_NAME)
+                LOGGER(__name__).info(f"Heroku App Configured")
             except BaseException:
                 LOGGER(__name__).warning(
                     f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
                 )
-async def main():
-    dbb()
-    await sudo()
-    heroku()
-asyncio.run(main())
