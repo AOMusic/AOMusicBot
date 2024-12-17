8a952f45c24d8cd688fee2e7e3967cb1935641c0
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-17 14:47:51 +0530

    Update misc.py

--- AnonXMusic/misc.py
+++ AnonXMusic/misc.py
@@ -1,24 +1,30 @@
 import socket
 import time
-
 import heroku3
 from pyrogram import filters
-
 import config
 from AnonXMusic.core.mongo import mongodb
-
 from .logging import LOGGER
 
+Sudoers Filter
 SUDOERS = filters.user()
 
+Heroku App
 HAPP = None
+
+Boot Time
 _boot_ = time.time()
 
-
 def is_heroku():
+    """
+    Check if the app is running on Heroku.
+    
+    Returns:
+        bool: True if the app is running on Heroku, False otherwise.
+    """
     return "heroku" in socket.getfqdn()
 
-
+Heroku Config Vars
 XCB = [
     "/",
     "@",
@@ -35,14 +41,18 @@ XCB = [
     "master",
 ]
 
-
 def dbb():
+    """
+    Initialize the local database.
+    """
     global db
     db = {}
     LOGGER(__name__).info(f"Local Database Initialized.")
 
-
 async def sudo():
+    """
+    Load sudoers from the database.
+    """
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
     sudoersdb = mongodb.sudoers
@@ -60,8 +70,10 @@ async def sudo():
             SUDOERS.add(user_id)
     LOGGER(__name__).info(f"Sudoers Loaded.")
 
-
 def heroku():
+    """
+    Configure the Heroku app.
+    """
     global HAPP
     if is_heroku:
         if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
@@ -72,4 +84,4 @@ def heroku():
             except BaseException:
                 LOGGER(__name__).warning(
                     f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
-                )
+)
