01ef51b55ed1ad8bd10928b1093aacc2d40bcea7
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-17 14:50:13 +0530

    Update logging.py

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -1,5 +1,6 @@
 import logging
 
+Logging Configuration
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -10,11 +11,20 @@ logging.basicConfig(
     ],
 )
 
+Suppressing Third-Party Library Logs
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
-
 def LOGGER(name: str) -> logging.Logger:
+    """
+    Returns a logger instance with the given name.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: A logger instance with the given name.
+    """
     return logging.getLogger(name)
