3b23b836d3b30f5c37960e8dcc22b924c216f44d
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-18 08:35:50 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -3,24 +3,20 @@ from AnonXMusic.core.dir import dirr
 from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
+
 from .logging import LOGGER
-#Initializing Directories
+
 dirr()
-
-#Initializing Database
+git()
 dbb()
-
-#Initializing Heroku
 heroku()
 
-#Initializing Bot and Userbot
 app = Anony()
 userbot = Userbot()
 
-#Importing Platforms
+
 from .platforms import *
 
-#Initializing Platforms
 Apple = AppleAPI()
 Carbon = CarbonAPI()
 SoundCloud = SoundAPI()
